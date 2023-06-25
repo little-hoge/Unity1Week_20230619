15677236db0b835700fa71b981c3903053ad2b05
little <little500ml@gmail.com>
2023-06-25 18:57:31 +0900

    タイトル修正

--- Assets/Script/Main/Game2/MiniGameController2.cs
+++ Assets/Script/Main/Game2/MiniGameController2.cs
@@ -14,7 +14,7 @@ namespace Unity1Week_20230619.Main.Game2
         public void Init()
         {
             base.Init(60);
-            Announce.text = "MiniGameController2 \nに、チャレンジ！";
+            Announce.text = "あめゲー \nに、チャレンジ！";
 
             playerController = miniGameObject.transform.Find("Basket").GetComponent<PlayerController>();
             fallObjectSpawner = miniGameObject.transform.Find("FallObjectSpawner").GetComponent<FallObjectSpawner>();
